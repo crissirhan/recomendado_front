59ca3c5cc1e2ef99ea93f881471bc0689f34dd70
Samir Saleh <samiruli@gmail.com>
2017-10-21 06:40:34 -0300

    anuncios esqueleto

--- src/components/AnnouncementPage.jsx
+++ src/components/AnnouncementPage.jsx
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom';
 import { Container, Col, Jumbotron, Button, Row } from 'reactstrap';
 import getAnnouncements from '../actions/get_announcements';
+import './css/images.css';
 
 
 class AnnouncementPage extends Component {
@@ -33,30 +34,31 @@ class AnnouncementPage extends Component {
 
   render(){
     if(!this.state.announcement){
-      return <Container></Container>
+      return <Container>Cargando</Container>
     }
     return (
       <Container>
         <Row>
           <Col sm="6" >
-            <Jumbotron>
               <h1 className="display-3">{this.state.announcement.title}</h1>
+              <img className="center-cropped announcement-thumbnail" src={this.state.announcement.announcement_thumbnail} alt="Imagen anuncio" />
               <p className="lead">{this.state.announcement.description}</p>
               <hr className="my-2" />
-              <p>${this.state.announcement.price}</p>
-              <p className="lead">
+              <p className="lead">Por: 
                 <Button color="link">
                   <Link to={'/profesionales/'+this.state.announcement.professional.id}>
                     {this.state.announcement.professional.user.first_name} {this.state.announcement.professional.user.last_name}
                   </Link>
                 </Button>
               </p>
-            </Jumbotron>
           </Col>
           <Col sm="6" >
-            <Jumbotron>
-              WIP
-            </Jumbotron>
+            <p>Precio: ${this.state.announcement.price}</p>
+            <p>Expira el: {new Date(this.state.announcement.expire_date).toLocaleDateString()}</p>
+            <p>Lugar: {this.state.announcement.location}</p>
+            <p>Movilidad: {this.state.announcement.movility}</p>
+            <p>Días de atención: {this.state.announcement.availability_display}</p>
+
           </Col>
         </Row>
       </Container>
